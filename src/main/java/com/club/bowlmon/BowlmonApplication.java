955f06fbd371abde4078f7b1817923a29986ceb7
Yoonjin <elodie@kakao.com>
2020-10-27 16:55:19 +0900

    Initial commit

---
+++ src/main/java/com/club/bowlmon/BowlmonApplication.java
@@ -0,0 +1,13 @@
+package com.club.bowlmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club/bowlmon/BowlmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.bowlmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
